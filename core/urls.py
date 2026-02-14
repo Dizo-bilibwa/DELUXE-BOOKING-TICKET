d1d19a7c173e811d02e9514b2d5f350656342ce5
unknown <ahmadibilibwa@gmail.com>
2026-02-14 13:39:16 +0300

    Fix deployment

--- core/urls.py
+++ core/urls.py
@@ -6,70 +6,44 @@ from .views import CreateBookingView
 from .views import download_ticket_pdf
 from .views import PaymentView
 
+from .views import trains_list, routes_list, travel_dates, ticket_classes
+from .views import api_routes, api_trains
+from .views import register_user
+from .views import RegisterView, LoginView
+
+
 
 
 urlpatterns = [
     path('trains/', trains_list),
-]
 
 
-from django.urls import path
-from .views import trains_list, routes_list, travel_dates, ticket_classes
-
-urlpatterns = [
     path('routes/', routes_list),
     path('trains/', trains_list),
     path('travel-dates/<int:train_id>/', travel_dates),
     path('ticket-classes/', ticket_classes),
-]
 
 
-
-from .views import api_routes, api_trains
-
-urlpatterns += [
     path('api/routes/', api_routes),
     path('api/trains/', api_trains),
-]
 
 
-from .views import register_user
-urlpatterns+=[
-    path('register/',register_user),
-]
+
+ path('register/',register_user),
 
 
-urlpatterns=[
-    path('register/',RegisterView.as_view(),name='register')
-]
+  path('register/',RegisterView.as_view(),name='register'),
 
-from django.urls import path
-from .views import RegisterView, LoginView
 
-urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'), 
     
-]
+    path('bookings/', BookingView.as_view()),
 
 
-urlpatterns = [
-
-path('bookings/', BookingView.as_view()),
-
-]
-
-from django.urls import path
-from .views import RegisterView, LoginView
-
-urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
-]
 
-from django.urls import path
-
-urlpatterns = [
 
 path('book/', CreateBookingView.as_view(), name='book'),
 ]
