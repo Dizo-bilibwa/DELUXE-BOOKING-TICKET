0d2cbaff72c62136e44711948243ae8ccb630432
unknown <ahmadibilibwa@gmail.com>
2026-03-01 00:51:36 +0300

    Fix case-sensitive login import for Vercel build

--- deluxe-frontend/src/App.js
+++ deluxe-frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login";
+import Login from "./login";
 import Register from "./Register";
 import Booking from "./Booking";
 import ProtectedRoute from "./ProtectedRoute";
